5e0767823483ced403ce72185346bc0522842edf
001simran <simranabhi2007@gmail.com>
2026-03-28 01:50:25 +0530

    fix vercel backend path

--- api/index.py
+++ api/index.py
@@ -1,4 +1,10 @@
+import sys
+import os
+
+# Add backend folder to Python path
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
 from backend.app.main import app
 
-# Vercel requires this handler
+# Vercel handler
 handler = app
