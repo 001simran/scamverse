46a0d15e55c5f980a7fae071bd4d68ae10c6d6bd
001simran <simranabhi2007@gmail.com>
2026-03-28 02:09:26 +0530

    news

--- api/index.py
+++ api/index.py
@@ -1,12 +1,8 @@
 import sys
 import os
 
-# Add backend folder to Python path
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
-from backend.app.main import app
+from backend.app.main import app as fastapi_app
 
-# Vercel expects 'app' (ASGI), not handler
-# Just expose it like this:
-
-app = app
+app = fastapi_app
